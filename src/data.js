2340658938fa44ff37d3b5cdb48baa8290905767
Pranav <pranavrajput1602@gmail.com>
2021-10-06 23:24:14 +0530

    added new member

--- src/data.js
+++ src/data.js
@@ -13,5 +13,12 @@ export const membersData = [
       github: "",
       linkedin: "",
     },
+    {
+      name: "Pranav Rajput",
+      quote: "member",
+      insta: "",
+      github: "",
+      linkedin: "",
+    },
     
   ];
