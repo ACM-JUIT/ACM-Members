9cfc62c749215eadbeb3b2ef36d650b1492ac534
Aarhan Khan <72205689+aarhank@users.noreply.github.com>
2022-09-30 03:12:18 +0530

    update

--- src/data.js
+++ src/data.js
@@ -3,14 +3,7 @@ export const membersData = [
       name: "Aarhan Khan",
       quote: "developer of this site",
       insta: "https://www.instagram.com/aarhan_._/",
-      github: "https://github.com/bdab-x",
-      linkedin: "https://www.linkedin.com/in/aarhan-k-62742b196/",
-    },
-	{
-      name: "Aarhan Khan",
-      quote: "ahh yess again",
-      insta: "https://www.instagram.com/aarhan_._/",
-      github: "https://github.com/bdab-x",
+      github: "https://github.com/aarhank",
       linkedin: "https://www.linkedin.com/in/aarhan-k-62742b196/",
     },
 	
