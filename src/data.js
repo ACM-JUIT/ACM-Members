4e7e3ebed85697ee3c171683dacbb827c1f97e56
Aditya <aadityasaini8008@gmail.com>
2022-10-26 23:58:42 +0530

    first commit

--- src/data.js
+++ src/data.js
@@ -111,4 +111,11 @@ export const membersData = [
     github: "https://github.com/Adityak215",
     linkedin: "https://www.linkedin.com/in/aditya-kumar-b3185024a/",
   },
+  {
+    name: "Aaditya",
+    quote: "Member of ACM JUIT",
+    insta: "",
+    github: "",
+    linkedin: "",
+  },
 ];
